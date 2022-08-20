ec904f9a97b1c4b2cb6e9b87a65ee3a38eb583f8
Dammy Richie <formatable2018@gmail.com>
2022-08-20 20:01:46 +0100

    onboarding k

--- lib/constants/image_constants.dart
+++ lib/constants/image_constants.dart
@@ -3,5 +3,8 @@ class ConstanceData {
   static const String baseImageUrl = "assets/images/";
   static const String baseSvgUrl = "assets/svgs/";
 
+// images
+  static const map = baseImageUrl + "map.png";
+
   static const arrow_forward = baseSvgUrl + "arrow_double_forward.svg";
 }
--- lib/modules/onboarding.dart
+++ lib/modules/onboarding.dart
@@ -25,68 +25,76 @@ class _OnboardingState extends State<Onboarding> {
       ),
       child: Scaffold(
         backgroundColor: primaryColor,
-        body: Container(
-          child: Stack(
-            children: [
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: EdgeInsets.only(bottom: 20.h),
-                  child: Container(
-                    height: 360.h,
-                    width: 350.w,
-                    decoration: BoxDecoration(
-                      color: theme.backgroundColor,
-                      borderRadius: BorderRadius.all(Radius.circular(30.sp)),
-                    ),
-                    child: Column(
-                      children: [
-                        SizedBox(height: 45.h),
-                        Text(
-                          "Enjoy the new exprience of\nchatting with new friends",
-                          textAlign: TextAlign.center,
-                          style: CustomTheme.semiLargeText(context),
-                        ),
-                        SizedBox(height: 16.h),
-                        Text(
-                          "Lorem ipsum dolor sit amet, consectetur\nadipiscing elit, sed do eiusmod tempor incididunt ut labore\net dolore magna aliqua. Ut enim",
-                          textAlign: TextAlign.center,
-                          style: CustomTheme.normalText(context).copyWith(
-                            color: darkGreyTextColor,
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.w400,
+        body: SafeArea(
+          top: true,
+          child: Container(
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Image.asset(ConstanceData.map),
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 20.h),
+                    child: Container(
+                      height: 360.h,
+                      width: 350.w,
+                      decoration: BoxDecoration(
+                        color: theme.backgroundColor,
+                        borderRadius: BorderRadius.all(Radius.circular(30.sp)),
+                      ),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 45.h),
+                          Text(
+                            "Enjoy the new exprience of\nchatting with new friends",
+                            textAlign: TextAlign.center,
+                            style: CustomTheme.semiLargeText(context),
                           ),
-                        ),
-                        SizedBox(height: 80.h),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 20.w),
-                          child: SlideAction(
-                            key: _swipeKey,
-                            elevation: 0,
-                            height: 75.h,
-                            innerColor: theme.backgroundColor,
-                            outerColor: theme.primaryColor,
-                            submittedIcon: SvgPicture.asset(
-                              ConstanceData.arrow_forward,
-                              color: backgroundColor,
+                          SizedBox(height: 16.h),
+                          Text(
+                            "Lorem ipsum dolor sit amet, consectetur\nadipiscing elit, sed do eiusmod tempor incididunt ut labore\net dolore magna aliqua. Ut enim",
+                            textAlign: TextAlign.center,
+                            style: CustomTheme.normalText(context).copyWith(
+                              color: darkGreyTextColor,
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w400,
                             ),
-                            text: "Swipe to start...",
-                            borderRadius: 40.sp,
-                            textStyle: CustomTheme.mediumText(context).copyWith(
-                              color: theme.backgroundColor,
-                              fontWeight: FontWeight.w500,
-                            ),
-                            onSubmit: () {
-                              // go to register
-                            },
                           ),
-                        )
-                      ],
+                          SizedBox(height: 80.h),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20.w),
+                            child: SlideAction(
+                              key: _swipeKey,
+                              elevation: 0,
+                              height: 75.h,
+                              innerColor: theme.backgroundColor,
+                              outerColor: theme.primaryColor,
+                              submittedIcon: SvgPicture.asset(
+                                ConstanceData.arrow_forward,
+                                color: backgroundColor,
+                              ),
+                              text: "Swipe to start...",
+                              borderRadius: 40.sp,
+                              textStyle:
+                                  CustomTheme.mediumText(context).copyWith(
+                                color: theme.backgroundColor,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              onSubmit: () {
+                                // go to register
+                              },
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
